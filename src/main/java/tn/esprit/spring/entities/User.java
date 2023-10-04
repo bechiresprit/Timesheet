9fab215ed4adb16189d141743e3e87b3ada1e5fe
bechir.mehrez <bechir.mehrez@esprit.tn>
2023-10-04 11:40:43 +0200

    ajout commentaire222

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -18,6 +18,7 @@ import javax.persistence.TemporalType;
 @Table(name = "T_USER")
 public class User implements Serializable {
 //modification test 
+//Modification2
 
 	private static final long serialVersionUID = 1L;
 
